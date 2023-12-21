6d6f5d9a5a6ce58ac0150cec3e7c1b5eb54b7b3a
MaxHaPa6oTe <ya.bulankin-2015@yandex.ru>
2023-12-21 22:02:05 +0300

    12

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,15 +1,47 @@
+'use client'
 import Link from "next/link";
 import React from "react";
 import './AppBar.css'
 
 const AppBar = () => {
+  const [bar,setBar] = React.useState(false)
+  const wrapRef = React.useRef<any>(null)
+  const handleClick = (e:Event) => {
+    if (wrapRef && !wrapRef.current?.contains(e.target)) {
+      setBar(false)
+  }
+}
+  React.useEffect(()=>{
+    document.addEventListener('mousedown',handleClick)
+  },[])
   return (
+    <>
       <div className="AppBar">
-        <Link href='/otmetki' className="shapka">Отметки</Link>
-        <Link href='/workers' className="shapka">Работники</Link>
-        <Link href='/tyrnikets' className="shapka">Турникеты</Link>
-        <Link href='/profile' className="shapka">Аккаунт</Link>
+        <button onClick={()=>setBar(!bar)}>xyi</button>
       </div>
+      {bar && <div className="menu" ref={wrapRef}>
+            <div className="close">
+              <i>МЕНЮ</i>
+              <span ref={wrapRef} style={{cursor:'pointer'}}
+               onClick={()=>setBar(!bar)}>
+                X
+              </span>
+              </div>
+              <hr className="otstyp"/>
+        <Link href='/otmetki' className="shapka" onClick={()=>setBar(!bar)}>
+          Отметки
+          </Link>
+        <Link href='/workers' className="shapka" onClick={()=>setBar(!bar)}>
+          Работники
+          </Link>
+        <Link href='/tyrnikets' className="shapka" onClick={()=>setBar(!bar)}>
+          Турникеты
+          </Link>
+        <Link href='/profile' className="shapka" onClick={()=>setBar(!bar)}>
+          Аккаунт
+          </Link>
+      </div>}
+      </>
   );
 };
 
