430a1f86865349775b7c0cd9eea067b8e397eba5
Max <ya.bulankin-2015@yandex.ru>
2023-12-28 14:41:39 +0300

    15

--- src/app/workers/worker/page.tsx
+++ src/app/workers/worker/page.tsx
@@ -6,9 +6,40 @@ import './addWorker.css'
 
 const addWorker = () => {
  
-   return <div>
-   
-    
+   return <div className="addWorker">
+   <form onSubmit={e=>e.preventDefault()}>
+    <h2>Новый работник</h2>
+    <label>
+        ФИО Сотрудника
+    </label>
+    <br/>
+    <input type="text"/>
+    <br/>
+    <label>
+        Отдел
+    </label>
+    <br/>
+    <input type="text"/>
+    <br/>
+   <label>
+        Телефон
+    </label>
+    <br/>
+    <input type="text"/>
+    <br/>
+    <label>
+        Номер карты
+    </label>
+    <br/>
+    <input type="text"/>
+    <br/>
+    <label>
+        Фото сотрудника
+    </label>
+    <br/>
+    <input type="file"/>
+    <button className="addRab">Добавить</button>
+    </form>
     </div>
    
 };
